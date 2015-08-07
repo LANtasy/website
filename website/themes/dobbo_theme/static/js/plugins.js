f597a4dd09f1070f4ecd8153d61f768bde680c37
cdobbyn <cdobbyn@gmail.com>
2015-08-06 23:01:03 -0700

    Added index, overrode body in base.html. Added flexslider, added a few placeholder images (yeah #3). Added the js, css, and html required to simply display the widget. Will need to modify further to create hero frame from original design.

--- website/themes/dobbo_theme/static/js/plugins.js
+++ website/themes/dobbo_theme/static/js/plugins.js
@@ -22,3 +22,53 @@
 }());
 
 // Place any jQuery/helper plugins in here.
+
+jQuery(function($){
+	"use strict";
+
+var CONVOCATION = window.CONVOCATION || {};
+
+/* ==================================================
+   Hero Flex Slider
+================================================== */
+	CONVOCATION.heroflex = function() {
+		$('.flexslider').each(function(){
+            var carouselInstance = $(this);
+            var carouselAutoplay = carouselInstance.attr("data-autoplay") == 'yes' ? true : false
+            var carouselPagination = carouselInstance.attr("data-pagination") == 'yes' ? true : false
+            var carouselArrows = carouselInstance.attr("data-arrows") == 'yes' ? true : false
+            var carouselDirection = carouselInstance.attr("data-direction") ? carouselInstance.attr("data-direction") : "horizontal"
+            var carouselStyle = carouselInstance.attr("data-style") ? carouselInstance.attr("data-style") : "fade"
+            var carouselSpeed = carouselInstance.attr("data-speed") ? carouselInstance.attr("data-speed") : "5000"
+            var carouselPause = carouselInstance.attr("data-pause") == 'yes' ? true : false
+
+            carouselInstance.flexslider({
+                animation: carouselStyle,
+                easing: "swing",
+                direction: carouselDirection,
+                slideshow: carouselAutoplay,
+                slideshowSpeed: carouselSpeed,
+                animationSpeed: 600,
+                initDelay: 0,
+                randomize: false,
+                pauseOnHover: carouselPause,
+                controlNav: carouselPagination,
+                directionNav: carouselArrows,
+                prevText: "",
+                nextText: "",
+                start: function () {
+                  $('.flex-caption').show();
+                  BackgroundCheck.init({
+                    targets: '.body',
+                    images: '.flexslider li.parallax'
+                  });
+                },
+                after: function () {
+                  BackgroundCheck.refresh();
+                  $('.flex-caption').show();
+                }
+            });
+		});
+	}
+});
+
