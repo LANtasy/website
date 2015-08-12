62123a43758a34a55dc4485d0581be5bc9969f72
cdobbyn <cdobbyn@gmail.com>
2015-08-12 00:57:04 -0700

    Big commit, but just mostly moved things around. Migrated convocation theme under website.apps.convocation. Moved common css/js/fonts/etc from vendor to static.common. Cleaned up settings.py. Updated gitignore. This cleaned up an invisible issue with staticfiles (django was just fixing the issue for us).

--- website/settings/settings.py
+++ website/settings/settings.py
@@ -176,16 +176,14 @@ CACHE_MIDDLEWARE_KEY_PREFIX = PROJECT_APP
 # Example: "http://media.lawrence.com/static/"
 STATIC_URL = "/static/"
 
-STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, "static"),
-
-    )
-
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = os.path.join(PROJECT_ROOT, STATIC_URL.strip("/"))
+STATIC_ROOT = os.path.join(PROJECT_ROOT, "../static")
+
+
+STATICFILES_DIRS = (os.path.join(PROJECT_ROOT, "../website/static"),)
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
