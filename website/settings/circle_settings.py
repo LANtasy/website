44ae30936ca32b95f56af20e3aa90b63bee87d9e
Alex White <alex.white@smartpager.net>
2015-08-17 19:53:19 -0700

    Setup postgres for circleci

--- website/settings/circle_settings.py
+++ website/settings/circle_settings.py
@@ -14,15 +14,11 @@ DATABASES = {
         # Add "postgresql_psycopg2", "mysql", "sqlite3" or "oracle".
         "ENGINE": "django.db.backends.postgresql_psycopg2",
         # DB name or path to database file if using sqlite3.
-        "NAME": "db.sqlite3",
+        "NAME": "circle_test",
         # Not used with sqlite3.
-        "USER": "",
+        "USER": "ubuntu",
         # Not used with sqlite3.
         "PASSWORD": "",
-        # Set to empty string for localhost. Not used with sqlite3.
-        "HOST": "",
-        # Set to empty string for default. Not used with sqlite3.
-        "PORT": "",
     }
 }
 
