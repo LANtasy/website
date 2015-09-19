91783f019645b864050de2cd4a7f0a2584af4327
Alex White <alex.white@smartpager.net>
2015-09-19 00:27:45 -0700

    Create badges on order handler closes #89

--- website/apps/salesbro/checkout.py
+++ website/apps/salesbro/checkout.py
@@ -1,6 +1,9 @@
 from __future__ import unicode_literals, absolute_import
 
 import logging
+from django.db import transaction
+from website.apps.badgebro.models import Badge
+from website.apps.salesbro.models import TicketOption
 
 logger = logging.getLogger(__name__)
 
@@ -12,4 +15,16 @@ def salesbro_order_handler(request, order_form, order):
     Relate all passes to original order
     Make passes available for registration/assignment
     """
-    pass
+    with transaction.atomic():
+
+        for item in order.items.all():
+            ticket_option = TicketOption.objects.get(sku=item.sku)
+            for x in range(0, item.quantity):
+
+                badge = Badge()
+                badge.order = order
+                badge.ticket = ticket_option
+                badge.save()
+
+        order.status = 2
+        order.save()
