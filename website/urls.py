0961ddf4b7e4c0acfc1fa7a76a967e00e0aefbf7
cdobbyn <cdobbyn@gmail.com>
2015-09-23 01:13:09 -0700

    Changed ticket from salesbro/tickets/ to tickets/. Added migration to salesbro to automatically add this link in "Draft" status. Closes #108

--- website/urls.py
+++ website/urls.py
@@ -11,64 +11,19 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
-    # Cartridge URLs.
+    # Shop URLs.
     url(r'^shop/checkout/$', 'cartridge.shop.views.checkout_steps', name='checkout_steps',
-       kwargs=dict(form_class=OrderForm)),
+        kwargs=dict(form_class=OrderForm)),
     url(r'^shop/', include('cartridge.shop.urls')),
     url(r'^account/orders/$', 'cartridge.shop.views.order_history', name='shop_order_history'),
-
     url(r'^zebra/', include('zebra.urls',  namespace="zebra",  app_name='zebra')),
-    url(r'^salesbro/', include('website.apps.salesbro.urls',  namespace='salesbro')),
+    url(r'^', include('website.apps.salesbro.urls',  namespace='salesbro')),
 
-    # We don't want to presume how your homepage works, so here are a
-    # few patterns you can use to set it up.
+    # Index URL
+    url(r"^$", direct_to_template, {"template": "index.html"}, name="home"),
 
-    # HOMEPAGE AS STATIC TEMPLATE
-    # ---------------------------
-    # This pattern simply loads the index.html template. It isn't
-    # commented out like the others, so it's the default. You only need
-    # one homepage pattern, so if you use a different one, comment this
-    # one out.
-
-    url("^$", direct_to_template, {"template": "index.html"}, name="home"),
-
-    # HOMEPAGE AS AN EDITABLE PAGE IN THE PAGE TREE
-    # ---------------------------------------------
-    # This pattern gives us a normal ``Page`` object, so that your
-    # homepage can be managed via the page tree in the admin. If you
-    # use this pattern, you'll need to create a page in the page tree,
-    # and specify its URL (in the Meta Data section) as "/", which
-    # is the value used below in the ``{"slug": "/"}`` part.
-    # Also note that the normal rule of adding a custom
-    # template per page with the template name using the page's slug
-    # doesn't apply here, since we can't have a template called
-    # "/.html" - so for this case, the template "pages/index.html"
-    # should be used if you want to customize the homepage's template.
-
-    # url("^$", "mezzanine.pages.views.page", {"slug": "/"}, name="home"),
-
-    # HOMEPAGE FOR A BLOG-ONLY SITE
-    # -----------------------------
-    # This pattern points the homepage to the blog post listing page,
-    # and is useful for sites that are primarily blogs. If you use this
-    # pattern, you'll also need to set BLOG_SLUG = "" in your
-    # ``settings.py`` module, and delete the blog page object from the
-    # page tree in the admin if it was installed.
-
-    # url("^$", "mezzanine.blog.views.blog_post_list", name="home"),
-
-    # MEZZANINE'S URLS
-    # ----------------
-    # ADD YOUR OWN URLPATTERNS *ABOVE* THE LINE BELOW.
-    # ``mezzanine.urls`` INCLUDES A *CATCH ALL* PATTERN
-    # FOR PAGES, SO URLPATTERNS ADDED BELOW ``mezzanine.urls``
-    # WILL NEVER BE MATCHED!
-
-    # If you'd like more granular control over the patterns in
-    # ``mezzanine.urls``, go right ahead and take the parts you want
-    # from it, and use them directly below instead of using
-    # ``mezzanine.urls``.
-    ("^", include("mezzanine.urls")),
+    # Mezzanine defaults URLs
+    (r"^", include("mezzanine.urls")),
 
 )
 
