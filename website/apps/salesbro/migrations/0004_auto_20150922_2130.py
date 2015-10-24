7e454c8f4f692a12969beb770c84a6cb067fbbe1
Dobbo <chris@lantasy.com>
2015-10-24 15:43:00 -0700

    Create 'Ticket Sales' group used in vendor portal.

---
+++ website/apps/salesbro/migrations/0004_auto_20150922_2130.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations
+from django.core.exceptions import ObjectDoesNotExist
+
+group_name = u'Ticket Sales'
+
+def add_ticket_sales(apps, schema_editor):
+    group = apps.get_model('auth', 'Group')
+    try:
+        group.objects.get(name=group_name)
+    except ObjectDoesNotExist:
+        new_group = group(name=group_name)
+        new_group.save()
+
+
+def remove_ticket_sales(apps, schema_editor):
+    group = apps.get_model('auth', 'Group')
+    try:
+        group_object = group.objects.get(name=group_name)
+        group_object.delete()
+    except ObjectDoesNotExist:
+        pass
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('salesbro', '0003_auto_20150908_0416'),
+    ]
+
+    operations = [
+        migrations.RunPython(add_ticket_sales, remove_ticket_sales),
+    ]
