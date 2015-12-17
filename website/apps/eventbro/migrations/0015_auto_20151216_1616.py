587900a7c9f52144c6b8335abb5263da59c51185
Dobbo <chris@lantasy.com>
2015-12-16 16:30:16 -0800

    Add migrations.

---
+++ website/apps/eventbro/migrations/0015_auto_20151216_1616.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+import website.apps.eventbro.models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('eventbro', '0014_auto_20151216_1436'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='event',
+            name='thumbnail',
+            field=models.ImageField(null=True, upload_to=website.apps.eventbro.models.rename_thumb, blank=True),
+        ),
+    ]
