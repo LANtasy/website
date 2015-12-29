65ec4e44be1aebc0344e0cb818918cede9b08bfa
Chris Dobbyn <cdobbyn@gmail.com>
2015-12-29 14:52:45 -0800

    Added change-password option based on django OOB form.

--- website/apps/userbro/urls.py
+++ website/apps/userbro/urls.py
@@ -8,5 +8,6 @@ logger = logging.getLogger(__name__)
 
 urlpatterns = patterns('website.apps.userbro.views',
     url(r'^$', 'user_detail', name='user_detail'),
+    url(r'^change-password/$', 'change_password', name='change_password'),
     url(r'^release-badge/$', 'user_release_badge', name='user_release_badge'),
 )
--- website/apps/userbro/views.py
+++ website/apps/userbro/views.py
@@ -1,9 +1,12 @@
 from braces.views import LoginRequiredMixin
+from django.contrib.auth import update_session_auth_hash
+from django.contrib.auth.forms import PasswordChangeForm
 from django.contrib.auth.models import User
 from django.contrib.messages.views import SuccessMessageMixin
 from django.core.urlresolvers import reverse_lazy
+from django.http import HttpResponseRedirect
 from django.shortcuts import render, redirect
-from django.views.generic import UpdateView, DetailView
+from django.views.generic import UpdateView, DetailView, FormView
 from website.apps.badgebro.models import Badge
 from website.apps.eventbro.models import Registration
 
@@ -27,12 +30,38 @@ class UserDetailView(LoginRequiredMixin, SuccessMessageMixin, UserView, UpdateVi
     )
 
 
+class ChangePasswordView(LoginRequiredMixin, SuccessMessageMixin, UserView, FormView):
+    template_name = 'userbro/change_password.html'
+    form_class = PasswordChangeForm
+    success_message = 'Successfully changed password'
+    success_url = reverse_lazy('userbro:user_detail')
+
+    def get_form(self, form_class=None):
+        """
+        Returns an instance of the form to be used in this view.
+        """
+        if form_class is None:
+            form_class = self.get_form_class()
+        return form_class(user=self.get_object(), **self.get_form_kwargs())
+
+    def form_valid(self, form):
+        form.save()
+        # Updating the password logs out all other sessions for the user
+        # except the current one if
+        # django.contrib.auth.middleware.SessionAuthenticationMiddleware
+        # is enabled.
+        update_session_auth_hash(self.request, form.user)
+        return HttpResponseRedirect(self.get_success_url())
+
+
+
+
 class UserReleaseBadgeView(LoginRequiredMixin, SuccessMessageMixin, UserView, DetailView):
     template_name = 'userbro/user_badge_release.html'
     success_message = 'Successfully released badge'
 
     def post(self, request, *args, **kwargs):
-        user = self.request.user
+        user = self.get_object()
         self.remove_badge_association(user=user)
         self.remove_registrations(user=user)
 
@@ -62,3 +91,4 @@ class UserReleaseBadgeView(LoginRequiredMixin, SuccessMessageMixin, UserView, De
 
 user_detail = UserDetailView.as_view()
 user_release_badge = UserReleaseBadgeView.as_view()
+change_password = ChangePasswordView.as_view()
