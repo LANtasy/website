3591175dc15038a0442ac781a750e600a5f9bc09
Chris Dobbyn <cdobbyn@gmail.com>
2015-12-29 23:19:39 -0800

    Removed broken chain of db migrations, replaced with a single migration. This change will affect anyone who's migrated eventbro past 0012. This requires a revert to 0012 then one further migrate.

--- website/apps/eventbro/migrations/0013_auto_20151229_2315.py
+++ website/apps/eventbro/migrations/0013_auto_20151229_2315.py
@@ -2,12 +2,14 @@
 from __future__ import unicode_literals
 
 from django.db import migrations, models
+import sorl.thumbnail.fields
+import website.apps.eventbro.models
 
 
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('eventbro', '0018_auto_20151216_2238'),
+        ('eventbro', '0012_auto_20151216_1012'),
     ]
 
     operations = [
@@ -15,6 +17,11 @@ class Migration(migrations.Migration):
             model_name='event',
             name='event_type',
             field=models.CharField(blank=True, max_length=3, null=True, choices=[('LAN', 'BYOC LAN'), ('MIN', 'Miniatures'), ('RPG', 'RPG'), ('TAB', 'Tabletop')]),
+        ),
+        migrations.AlterField(
+            model_name='event',
+            name='image',
+            field=sorl.thumbnail.fields.ImageField(null=True, upload_to=website.apps.eventbro.models.rename_image, blank=True),
         ),
         migrations.AlterField(
             model_name='registration',
