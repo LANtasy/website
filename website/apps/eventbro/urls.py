9e46df1cf10ceeca173995387c783f6760176aea
Chris Dobbyn <cdobbyn@gmail.com>
2016-01-08 02:32:45 -0800

    First steps to adding event/eventtype/convention detailed views.

--- website/apps/eventbro/urls.py
+++ website/apps/eventbro/urls.py
@@ -13,5 +13,9 @@ urlpatterns = patterns('website.apps.eventbro.views',
     url(r'^register/badge/$', 'register_badge', name='register_badge'),
     url(r'^register/events/$', 'register_event', name='register_event'),
     url(r'^register/events/(?P<slug>[-\w]+)/$', 'register_event', name='register_event'),
-    url(r'^register/events/(?P<event_slug>[-\w]+)/update/$', 'registration_detail', name='registration_update')
+    url(r'^register/events/(?P<event_slug>[-\w]+)/update/$', 'registration_detail', name='registration_update'),
+
+    url(r'^convention/(?P<slug>[-\w]+)/$', 'convention_detail', name='convention_detail'),
+    url(r'^eventtype/(?P<slug>[-\w]+)/$', 'event_type_detail', name='event_type_detail'),
+    url(r'^event/(?P<slug>[-\w]+)/$', 'event_detail', name='event_detail'),
 )
