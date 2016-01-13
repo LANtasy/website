8627ae5f02574e9f00dcf2c497d295a6293ca0ac
Chris Dobbyn <cdobbyn@gmail.com>
2016-01-13 01:44:53 -0800

    Cleaned up context processor. Still not very efficient but it works.

--- website/apps/eventbro/context_processors.py
+++ website/apps/eventbro/context_processors.py
@@ -1,16 +1,16 @@
 from website.apps.eventbro.models import Event, Convention, EventType
 
 
-def events_navbar(request):
-    conventions = Convention.objects.filter(published=True)
-    conventions = conventions.only('name', 'slug', 'start')
-    conventions = conventions.order_by('start')
+def event(request):
+    try:
+        convention = Convention.objects.get(active=True, published=True)
+        events = Event.objects.filter(convention=convention, published=True)
+        events = events.order_by('event_type__name', '-showcase', 'name')
 
-    # events = Event.objects.filter(published=True, convention__published=True)
-    # events = events.only('convention', 'event_type', 'name', 'slug', 'start')
-    # events = events.order_by('convention__start', 'event_type', 'start', 'name')
-    return {
-        # 'navbar_events': events,
-        'navbar_conventions': conventions,
-    }
-
+        return {
+            'navbar_convention': convention,
+            'navbar_events': events,
+        }
+    except Convention.DoesNotExist:
+        # Don't do anything
+        return {}
