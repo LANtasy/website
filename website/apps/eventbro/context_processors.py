05bfbd94f807bbb3d0eafad7a67e129d1b87eb01
Chris Dobbyn <cdobbyn@gmail.com>
2016-01-13 21:10:38 -0800

    Cleanup leftover comments

--- website/apps/eventbro/context_processors.py
+++ website/apps/eventbro/context_processors.py
@@ -6,12 +6,9 @@ def event(request):
         convention = Convention.objects.get(active=True, published=True)
         event_types = EventType.objects.filter(event_type_id__convention=convention)
         event_types = event_types.values('name', 'slug').distinct()
-        # events = Event.objects.filter(convention=convention, published=True)
-        # events = events.order_by('event_type__name', '-showcase', 'name')
 
         return {
             'navbar_convention': convention,
-            # 'navbar_events': events,
             'navbar_event_types': event_types,
         }
     except Convention.DoesNotExist:
