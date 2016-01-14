ac6d2455635e25c4da89f1dde774206bb31f8bc8
Chris Dobbyn <cdobbyn@gmail.com>
2016-01-13 21:04:46 -0800

    Made nav only display categories.

--- website/apps/eventbro/context_processors.py
+++ website/apps/eventbro/context_processors.py
@@ -4,12 +4,15 @@ from website.apps.eventbro.models import Event, Convention, EventType
 def event(request):
     try:
         convention = Convention.objects.get(active=True, published=True)
-        events = Event.objects.filter(convention=convention, published=True)
-        events = events.order_by('event_type__name', '-showcase', 'name')
+        event_types = EventType.objects.filter(event_type_id__convention=convention)
+        event_types = event_types.values('name', 'slug').distinct()
+        # events = Event.objects.filter(convention=convention, published=True)
+        # events = events.order_by('event_type__name', '-showcase', 'name')
 
         return {
             'navbar_convention': convention,
-            'navbar_events': events,
+            # 'navbar_events': events,
+            'navbar_event_types': event_types,
         }
     except Convention.DoesNotExist:
         # Don't do anything
