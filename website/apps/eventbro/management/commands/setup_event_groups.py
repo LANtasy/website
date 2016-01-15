a4b9992218bbbda5b9e8f3e3f4e4aa8ef5547e95
Chris Dobbyn <cdobbyn@gmail.com>
2016-01-15 02:23:54 -0800

    Enhanced this command to add 3 different but equally important sets of permissions. For Bonnie/Angela to allow manual add of Registrations.This command must be run manually before the groups will appear.

--- website/apps/eventbro/management/commands/setup_event_groups.py
+++ website/apps/eventbro/management/commands/setup_event_groups.py
@@ -10,20 +10,35 @@ class Command(BaseCommand):
         self.add_event_editor()
 
     def add_event_editor(self):
-        group_name = 'Event editor'
-        permissions = ['add_event', 'change_event', 'delete_event']
+        group_permissions = [
+            {
+                'name': 'Event editor',
+                'permissions': ['add_event', 'change_event']
+            },
+            {
+                'name': 'Event admin',
+                'permissions': ['add_event', 'change_event', 'delete_event', 'delete_registration']
+            },
+            {
+                'name': 'Registration editor',
+                'permissions': ['add_registration', 'change_registration']
+            },
+        ]
 
-        try:
-            group = Group.objects.get(name=group_name)
-        except Group.DoesNotExist:
-            group = Group(name=group_name)
-            group.save()
-
-        for permission_name in permissions:
+        for group_permission in group_permissions:
             try:
-                permission = Permission.objects.get(codename=permission_name)
-                group.permissions.add(permission)
-            except Permission.DoesNotExist, e:
-                raise e
-            except Group.DoesNotExist, e:
-                raise e
+                group = Group.objects.get(name=group_permission.get('name'))
+            except Group.DoesNotExist:
+                group = Group(name=group_permission.get('name'))
+                group.save()
+
+            for permission_name in group_permission.get('permissions'):
+                try:
+                    permission = Permission.objects.get(codename=permission_name)
+                    group.permissions.add(permission)
+                except Permission.DoesNotExist, e:
+                    raise e
+                except Group.DoesNotExist, e:
+                    raise e
+
+
