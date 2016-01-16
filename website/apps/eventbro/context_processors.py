11c1833f692532f68d31678804fedcf429b37e59
Chris Dobbyn <cdobbyn@gmail.com>
2016-01-16 15:11:31 -0800

    Made context processor use convention.get_event_types() model so we are reproducing code elsewhere.

--- website/apps/eventbro/context_processors.py
+++ website/apps/eventbro/context_processors.py
@@ -4,8 +4,7 @@ from website.apps.eventbro.models import Event, Convention, EventType
 def event(request):
     try:
         convention = Convention.objects.get(active=True, published=True)
-        event_types = EventType.objects.filter(event_type_id__convention=convention)
-        event_types = event_types.values('name', 'slug').distinct()
+        event_types = convention.get_event_types()
 
         return {
             'navbar_convention': convention,
