637cc58a12455c380b78fa12c5c02813bc1a6b60
Dobbo <chris@lantasy.com>
2016-02-28 13:49:36 -0800

    Fixes #275 - Added search fields.

--- website/apps/badgebro/admin.py
+++ website/apps/badgebro/admin.py
@@ -5,10 +5,13 @@ from website.apps.badgebro.models import Badge, UpgradeTransaction
 
 
 class BadgeAdmin(admin.ModelAdmin):
-    list_display = product_list_display = ["uid", "ticket", "order", "user"]
+    #list_display = product_list_display = ['uid', 'ticket', 'order', 'user']
+    list_display = product_list_display = ['uid', 'order', 'user', 'type', 'option', 'network']
 
-    list_filter = ("order", "ticket")
+    list_filter = ('order', 'ticket')
     readonly_fields = ('uid', )
+
+    search_fields = ['uid', 'network', 'first_name', 'last_name', 'type', 'option']
 
 
 class UpgradeTransactionAdmin(admin.ModelAdmin):
