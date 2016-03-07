1c779f909fa4bd79c3ff6a259ffb6b231bc80c05
Chris Dobbyn <cdobbyn@gmail.com>
2016-03-07 02:11:26 -0800

    Simple badge creation functionality for trusted staff. This is to be used by trusted staff to generate staff/vendor badges as needed for the event.

--- website/apps/badgebro/views.py
+++ website/apps/badgebro/views.py
@@ -14,11 +14,11 @@ from django.http import JsonResponse, Http404, HttpResponse
 from django.shortcuts import get_object_or_404
 
 # Create your views here.
-from django.views.generic import ListView, UpdateView, DetailView
+from django.views.generic import ListView, UpdateView, DetailView, CreateView
 from django.views.generic.list import BaseListView
 from extra_views import ModelFormSetView
 
-from website.apps.badgebro.forms import BadgeUpdateForm, BadgeUpgradeForm
+from website.apps.badgebro.forms import BadgeUpdateForm, BadgeUpgradeForm, BadgeCreateForm
 from website.apps.badgebro.models import Badge, UpgradeTransaction
 from website.apps.eventbro.models import Convention, Event, Registration
 from website.apps.salesbro.models import TicketOption
@@ -423,12 +423,26 @@ class OrganizeBadgesExportView(OrganizeBadgesListView):
         return response
 
 
+class BadgeCreateView(GroupRequiredMixin, SuccessMessageMixin, CreateView):
+    group_required = 'trusted'
+    model = Badge
+    template_name = 'badgebro/badge_create.html'
+    success_message = 'Successfully created badge'
+    form_class = BadgeCreateForm
+
+    def get_success_url(self):
+        uid = self.object.uid
+        url = reverse('badgebro:badge_detail', kwargs={'uid': uid})
+        return url
+
+
 front_desk = FrontDeskListView.as_view()
 badge_detail = BadgeDetailView.as_view()
 badge_upgrade = BadgeUpgradeView.as_view()
 badge_print = BadgePrintView.as_view()
 badge_print_bulk = BadgeBulkPrintView.as_view()
 badge_print_close = BadgePrintCloseView.as_view()
+badge_create = BadgeCreateView.as_view()
 
 badge_order_upgrade = BadgeOrderUgradeView.as_view()
 badge_printed = BadgeSetPrintedView.as_view()
