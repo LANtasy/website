b65bc487b640fbd139b67cda54cc5a516c705223
Dobbo <chris@lantasy.com>
2016-03-08 13:07:29 -0800

    Fixed ordering of registration export.

--- website/apps/badgebro/views.py
+++ website/apps/badgebro/views.py
@@ -331,6 +331,7 @@ class OrganizeEventRegistrationsExportView(OrganizeEventRegistrationsListView):
 class OrganizeRegistrationsListView(GroupRequiredMixin, ListView):
     group_required = u'frontdesk'
     queryset = Registration.objects.all()
+    ordering = ['event', 'date_added']
     template_name = 'badgebro/organize/all_registrations.html'
 
 
